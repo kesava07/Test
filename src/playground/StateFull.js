d747f177e0de0e8481631df8b9a7b8d419c9324a
kesava07 <kesava07.ip@gmail.com>
2020-06-13 16:29:45 +0530

    statefull commit

---
+++ src/playground/StateFull.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StateFull extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hey this is a state full component</h1>
+            </div>
+        )
+    }
+}
